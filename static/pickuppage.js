982301c7224a655ae9fa3f8d402ac681a3ec584c
anshium <anshchablani@gmail.com>
2023-05-07 18:43:14 +0530

    Changes

--- static/pickuppage.js
+++ static/pickuppage.js
@@ -6,13 +6,7 @@ const orderer_index = 4
 const location_index = 5
 const batch_index = 6
 
-let to_remove = document.getElementsByClassName("remove")
-
 function remove(id){
-	let tab = document.getElementById("tabli")
-	let d_nested = document.getElementById(id);
-	tab.removeChild(d_nested);
+	let to_change = document.getElementById(id)
+	to_change.innerHTML = "Accepted"
 }
-
-for(let i = 0; i < to_remove.length; i++)
-to_remove[i].addEventListener('click', remove(4))
