7c9bbb30062cf4aa5125a631580ca2ad8fd6c7a2
anshium <anshchablani@gmail.com>
2023-05-07 18:16:30 +0530

    Hello

--- static/pickuppage.js
+++ static/pickuppage.js
@@ -5,3 +5,14 @@ const quantity_index = 3
 const orderer_index = 4
 const location_index = 5
 const batch_index = 6
+
+let to_remove = document.getElementsByClassName("remove")
+
+function remove(id){
+	let tab = document.getElementById("tabli")
+	let d_nested = document.getElementById(id);
+	tab.removeChild(d_nested);
+}
+
+for(let i = 0; i < to_remove.length; i++)
+to_remove[i].addEventListener('click', remove(4))
