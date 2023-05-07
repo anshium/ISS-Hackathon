0c374b86b7618eb8fdcac2c241ee5bfedf9b4a7d
anshium <anshchablani@gmail.com>
2023-05-07 14:52:39 +0530

    Flask port

--- runscript.py
+++ runscript.py
@@ -6,31 +6,27 @@ import json
 app = Flask(__name__)
 CORS(app, origins="*")
 
-@app.route("/")
-def foo():
-    return '''<h1>You are at the root page of this flask server</h1>'''
-
-@app.route("/pickup", methods = ['POST'])
-def getTheMadeRequest():
-	data = request.get_json()
-	person_name = data.name
-	person_addr = data.address
-	items = data.items
-	hash = 10
-	conn = sqlite3.connect("database.db")
-	cur = conn.cursor()
-	table_name = "orders"
-	for i in items:
-		canteen_id = i.canteen_id
-		item_id = i.item_id
-		quantity = i.quantity
-		cur.execute(f'''
-			INSERT INTO 
-			{table_name}(hash, person_name, person_address, canteed_id, item_id, quantity) 
-			VALUES({hash}, {person_name}, {person_addr}, {canteen_id}, {item_id}, {quantity})
-		''')
-	cur.commit()
-	cur.close()
+# @app.route("/pickup", methods = ['POST'])
+# def getTheMadeRequest():
+# 	data = request.get_json()
+# 	person_name = data.name
+# 	person_addr = data.address
+# 	items = data.items
+# 	hash = 10
+# 	conn = sqlite3.connect("database.db")
+# 	cur = conn.cursor()
+# 	table_name = "orders"
+# 	for i in items:
+# 		canteen_id = i.canteen_id
+# 		item_id = i.item_id
+# 		quantity = i.quantity
+# 		cur.execute(f'''
+# 			INSERT INTO 
+# 			{table_name}(hash, person_name, person_address, canteed_id, item_id, quantity) 
+# 			VALUES({hash}, {person_name}, {person_addr}, {canteen_id}, {item_id}, {quantity})
+# 		''')
+# 	cur.commit()
+# 	cur.close()
 
 @app.route("/receive", methods = ['GET', 'POST'])
 def bar():
@@ -52,6 +48,18 @@ def bar():
 @app.route("/pickup")
 def pickUpPage():
 	return render_template("pickuppage.html")
+
+@app.route("/")
+def homepage():
+	return render_template("homepage.html")
+
+@app.route("/aboutpage")
+def aboutpage():
+	return render_template("aboutpage.html")
+
+@app.route("/menupage")
+def menupage():
+	return render_template("menu_page.html")
 
 if __name__ == "__main__":
 	app.run(debug = True)
